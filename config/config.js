7f36397d9aaec0f43391ebfc2212a032db8ace8b
Empire Démocratique du Poulpe <alexis.lecomte@supinfo.com>
2021-05-09 18:43:18 +0200

    Added Mail service

--- config/config.js
+++ config/config.js
@@ -15,6 +15,18 @@ const config = {
 	},
 	api: {
 		prefix: "/api"
+	},
+	oauth2: {
+		clientId: "571942492534-gc8fi36bqt6ojl2uf91ghiirsqcode0k.apps.googleusercontent.com",
+		clientSecret: "qqlpt7wyQa5ZvEmrXbBZrsnA",
+		refreshToken: "1//04l38gnSaGb9TCgYIARAAGAQSNwF-L9IrGSeluw8KgVkKdAOJXjkPswH-GAaErgOEuEB4NE2CytEZsEBj5a3onXivBk5v9I99oyg"
+	},
+	email: {
+		service: "gmail",
+		host: "smtp.gmail.com",
+		port: "465",
+		address: "noreply.goodfork@gmail.com",
+		password: "Is This A S4lad?!"
 	}
 };
 
---
+++ global/Mail.js
@@ -0,0 +1,55 @@
+import nodemailer from "nodemailer";
+import { google } from "googleapis";
+import config from "../config/config.js";
+
+/* ---- Configure ------------------------------- */
+const { oauth2, email } = config;
+
+/* -- OAuth2 */
+const OAuth2Client = new google.auth.OAuth2(oauth2.clientId, oauth2.clientSecret);
+OAuth2Client.setCredentials({ refresh_token: oauth2.refreshToken });
+
+const accessToken = OAuth2Client.getAccessToken();
+
+/* -- Nodemailer */
+const transporter = nodemailer.createTransport({
+	service: email.service,
+	auth: {
+		type: "OAuth2",
+		user: email.address,
+		clientId: oauth2.clientId,
+		clientSecret: oauth2.clientSecret,
+		refreshToken: oauth2.refreshToken,
+		accessToken: accessToken
+	},
+	tls: { rejectUnauthorized: false }
+});
+
+transporter.verify()
+	.then(() => console.log("Ready to send email."))
+	.catch(err => console.error(err));
+
+/* ---- Pre-defined emails ---------------------- */
+function sendPassword(targetEmail, password) {
+	const mailContent = {
+		from: email.address,
+		to: targetEmail,
+		subject: "The Good Fork - Bienvenue dans l'équipe !",
+		text: `Vous pouvez dès à présent vous connecter sur l'application The Good Fork avec les identifiants suivants :\n
+		Identifiant : ${targetEmail}\n
+		Mot de passe: ${password}\n\n
+		N'oubliez pas de changer le mot de passe après votre première connexion.`,
+		html: `<p>Vous pouvez dès à présent vous connecter sur l'application The Good Fork avec les identifiants suivants :</p>
+		<ul>
+			<li>Identifiant : ${targetEmail}</li>
+			<li>Mot de passe: ${password}</li>
+		</ul>
+		<p>N'oubliez pas de changer le mot de passe après votre première connexion.</p>`
+	};
+
+	return transporter.sendMail(mailContent);
+}
+
+/* ---- Export ---------------------------------- */
+const Mail = { sendPassword };
+export default Mail;
