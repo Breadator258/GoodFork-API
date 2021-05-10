1a73de252ca78774b859255a6e09b583b03f1574
Empire Démocratique du Poulpe <alexis.lecomte@supinfo.com>
2021-05-10 15:47:34 +0200

    Fixed import

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import users from "./routes/users.js";
 import stock from "./routes/stock.js";
-import tables from "./routes/tables";
+import tables from "./routes/tables.js";
 
 export default () => {
 	const router = Router();
