1be6ace68f93391bfe20584ec34aa52ec6c5dce4
Empire Démocratique du Poulpe <alexis.lecomte@supinfo.com>
2021-05-15 18:55:13 +0200

    Added CRUD methods for menu ingredients

--- api/models/Menu.js
+++ api/models/Menu.js
@@ -1,9 +1,38 @@
+import { getFieldsToUpdate } from "../../global/Functions.js";
 import Stock from "./Stock.js";
 import ModelError from "../../global/ModelError.js";
 
 /*****************************************************
+ * Checkers
+ *****************************************************/
+
+const areUnitsValid = units => {
+	return units >= 0;
+};
+
+/*****************************************************
  * CRUD Methods
  *****************************************************/
+/* ---- CREATE ---------------------------------- */
+const addIngredient = async (db, menu_id, name, units, units_unit_id) => {
+	if (!areUnitsValid(units)) {
+		return new ModelError(400, "You must provide a valid quantity.", ["units"]);
+	}
+
+	let stockItem = await Stock.getByName(db, name);
+	let stockId = stockItem ? stockItem.stock_id : null;
+
+	if (!stockId) {
+		const newItem = await Stock.add(db, name, 0, units_unit_id, 0, false, false, null, null);
+		stockId = newItem.insertId;
+	}
+
+	return db.query(`
+		INSERT INTO menu_ingredients(menu_id, stock_id, units, units_unit_id)
+		VALUES (?, ?, ?, ?)
+	`, [menu_id, stockId, units, units_unit_id]
+	);
+};
 
 /* ---- READ ------------------------------------ */
 const getAll = async db => {
@@ -12,10 +41,14 @@ const getAll = async db => {
 			menus.menu_id,
 			menus.name,
 			menus.description,
+			mi.ingredient_id,
 			mi.stock_id,
-			mi.units
+			mi.units,
+			units.unit_id AS "units_unit_id",
+			units.name AS "units_unit"
 		FROM menus
 		LEFT JOIN menu_ingredients mi ON menus.menu_id = mi.menu_id
+		LEFT JOIN units ON mi.units_unit_id = units.unit_id
 		ORDER BY menus.menu_id
 	`);
 
@@ -28,10 +61,14 @@ const getById = async (db, menu_id) => {
 			menus.menu_id,
 			menus.name,
 			menus.description,
+			mi.ingredient_id,
 			mi.stock_id,
-			mi.units
+			mi.units,
+			units.unit_id AS "units_unit_id",
+			units.name AS "units_unit"
 		FROM menus
 		LEFT JOIN menu_ingredients mi ON menus.menu_id = mi.menu_id
+		LEFT JOIN units ON mi.units_unit_id = units.unit_id
 		WHERE menus.menu_id = ?
 	`, [menu_id]);
 
@@ -59,10 +96,12 @@ const buildFullMenus = async (db, menus) => {
 		const stock = await Stock.getById(db, menu.stock_id);
 
 		fullMenu.ingredients.push({
+			ingredient_id: menu.ingredient_id,
 			stock_id: stock.stock_id,
 			name: stock.name,
 			units: menu.units,
-			units_unit: stock.units_unit
+			units_unit: menu.units_unit,
+			units_unit_id: menu.units_unit_id
 		});
 
 		fullMenus.set(menu.menu_id, fullMenu);
@@ -71,9 +110,33 @@ const buildFullMenus = async (db, menus) => {
 	return Array.from(fullMenus).map(([_, menu]) => menu);
 };
 
+/* ---- UPDATE ---------------------------------- */
+const updateIngredient = async (db, ingredient_id, name, units, units_unit_id) => {
+	if (!areUnitsValid(units)) {
+		return new ModelError(400, "You must provide a valid quantity.", ["units"]);
+	}
+
+	const stockItem = Stock.getByName(db, name);
+	let stockId = stockItem ? stockItem.stock_id : null;
+
+	if (!stockId) {
+		const newItem = await Stock.add(db, name, 0, units_unit_id, 0, false, false, null, null);
+		stockId = newItem.insertId;
+	}
+
+	const updatingFields = getFieldsToUpdate({ stock_id: stockId, units, units_unit_id });
+
+	return db.query(`UPDATE menu_ingredients SET ${updatingFields} WHERE ingredient_id = ?`, [ingredient_id]);
+};
+
+/* ---- DELETE ---------------------------------- */
+const delIngredient = async (db, ingredient_id) => {
+	return db.query("DELETE FROM menu_ingredients WHERE ingredient_id = ?", [ingredient_id]);
+};
+
 /*****************************************************
  * Export
  *****************************************************/
 
-const Menu = { getById, getAll };
+const Menu = { addIngredient, getById, getAll, updateIngredient, deleteIngredient: delIngredient };
 export default Menu;
--- api/routes/menus.js
+++ api/routes/menus.js
@@ -8,6 +8,30 @@ const route = Router();
 export default (router) => {
 	router.use("/menus", route);
 
+	/* ---- CREATE ---------------------------------- */
+	route.post(
+		"/ingredients",
+		middlewares.checkParams("menu_id", "name", "units", "units_unit_id"),
+		middlewares.database,
+		async (request, response) => {
+			const { menu_id, name, units, units_unit_id } = request.body;
+			const db = await request.database;
+
+			response.set("Content-Type", "application/json");
+
+			Menu.addIngredient(db, menu_id, name, units, units_unit_id)
+				.then(result => {
+					if (result instanceof ModelError) {
+						response.status(result.code()).json(result.json()).end();
+					} else {
+						response.status(202).json({ code: 202, message: "Ingredient added to the menu." }).end();
+					}
+				})
+				.catch(err => response.status(500).json(new ModelError(500, err.message).json()).end())
+				.finally(() => db ? db.release() : null);
+		}
+	);
+
 	/* ---- READ ------------------------------------ */
 	route.get(
 		"/all",
@@ -45,6 +69,54 @@ export default (router) => {
 						response.status(result.code()).json(result.json()).end();
 					} else {
 						response.status(200).json({ code: 200, menu: result }).end();
+					}
+				})
+				.catch(err => response.status(500).json(new ModelError(500, err.message).json()).end())
+				.finally(() => db ? db.release() : null);
+		}
+	);
+
+	/* ---- UPDATE ------------------------------------ */
+	route.put(
+		"/ingredients",
+		middlewares.checkParams("ingredient_id"),
+		middlewares.database,
+		async (request, response) => {
+			const { ingredient_id, name, units, units_unit_id } = request.body;
+			const db = await request.database;
+
+			response.set("Content-Type", "application/json");
+
+			Menu.updateIngredient(db, ingredient_id, name, units, units_unit_id)
+				.then(result => {
+					if (result instanceof ModelError) {
+						response.status(result.code()).json(result.json()).end();
+					} else {
+						response.status(202).json({ code: 202, message: "Ingredient updated." }).end();
+					}
+				})
+				.catch(err => response.status(500).json(new ModelError(500, err.message).json()).end())
+				.finally(() => db ? db.release() : null);
+		}
+	);
+
+	/* ---- DELETE ------------------------------------ */
+	route.delete(
+		"/ingredients",
+		middlewares.checkParams("ingredient_id"),
+		middlewares.database,
+		async (request, response) => {
+			const { ingredient_id } = request.body;
+			const db = await request.database;
+
+			response.set("Content-Type", "application/json");
+
+			Menu.deleteIngredient(db, ingredient_id)
+				.then(result => {
+					if (result instanceof ModelError) {
+						response.status(result.code()).json(result.json()).end();
+					} else {
+						response.status(202).json({ code: 202, message: "Ingredient removed from the menu." }).end();
 					}
 				})
 				.catch(err => response.status(500).json(new ModelError(500, err.message).json()).end())
